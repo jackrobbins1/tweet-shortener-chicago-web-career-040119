1b595e725f768eb55d58626824ed6ad4405f05e7
Jack Robbins <jackrobbins1@gmail.com>
2019-03-22 02:05:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+  hash
+end
+
+def word_substituter(str, hash = dictionary)
+  newStr = ""
+  arr = str.split(" ")
+  #binding.pry
+  arr.each do |x|
+      if hash.has_key?(x)
+        newStr += hash[x] + " "
+      else
+        newStr += x + " "
+      end
+  end
+  newStr.chomp(" ")
+end
+
+tweet = "Hey guys, can anyone teach me how to be cool?"
+#word_substituter(tweet)
+
+def bulk_tweet_shortener(arr, method = word_substituter)
+  arr.each do |str|
+    puts method(str)
+  end
+
+  # x = method(str)
+  # binding.pry
+  # puts x
+end
+
+#bulk_tweet_shortener(tweet)
